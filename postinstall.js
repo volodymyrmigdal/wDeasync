bbc1b959aebf8fde6daa6afe5201c3819e340ec9
volodymyrmigdal <volodymyr.migdal@gmail.com>
2020-11-09 16:53:21 +0200

    update styles

--- postinstall.js
+++ postinstall.js
@@ -1,31 +1,43 @@
 #!/usr/bin/env node
+( function _postinstall_() {
+  
+  'use strict';
+  
+  let ChildProcess = require( 'child_process' );
 
-let cp = require('child_process');
+  ChildProcess.execFile( process.execPath, [ 'quick-test.js' ], ( err, stdout, stderr ) =>
+  {
+    if ( err || stderr ) 
+    {
+      console.log( 'Problem with the binary; manual build incoming' );
+      console.log( 'stdout=' + stdout );
+      console.log( 'err=' + err );
+      build();
+    }
+    else 
+    {
+      console.log( 'Binary is fine; exiting' );
+    }
+  });
+  
+  /* */
 
-cp.execFile( process.execPath, [ 'quick-test.js' ], function( err, stdout, stderr ) 
-{
-	if ( err || stderr ) 
-	{
-		console.log( 'Problem with the binary; manual build incoming' );
-		console.log( 'stdout=' + stdout );
-		console.log( 'err=' + err );
-		build();
-	}
-	else 
-	{
-		console.log( 'Binary is fine; exiting' );
-	}
-});
-
-function build() 
-{
-	let pnd = cp.spawn( 'npm', [ 'run', 'node-pre-gyp-build' ], { stdio: 'inherit', cwd : __dirname })
-	
-	pnd.on( 'exit', function( exitCode ) 
-	{
-		if( exitCode === 0 )
-		return;
-		console.error( 'Build failed' );
-		return process.exit( exitCode );
-	});
-}
+  function build() 
+  {
+    let o = 
+    { 
+      stdio: 'inherit', 
+      cwd : __dirname 
+    }
+    
+    let pnd = ChildProcess.spawn( 'npm', [ 'run', 'node-pre-gyp-build' ], o )
+    
+    pnd.on( 'exit', function( exitCode ) 
+    {
+      if( exitCode === 0 )
+      return;
+      console.error( 'Build failed' );
+      return process.exit( exitCode );
+    });
+  }
+})();
